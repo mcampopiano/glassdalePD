3d01b59942ae8b378337522359237ca960d2c1ee
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-20 15:34:34 -0500

    Added eventHub events to have the dropdown filter the criminals based on their crimes

--- scripts/criminals/criminalList.js
+++ scripts/criminals/criminalList.js
@@ -1,13 +1,17 @@
 import {getCriminals, useCriminals} from "./criminalProvider.js"
 import {CriminalHtml} from "./criminal.js"
+import { useConvictions } from "../convictions/ConvictionProvider.js"
 
-// renders the criminal cards to the DOM
+// ******************MY CODE BEFORE REFACTORING FOR EVENT HUB ****************
+
+// // renders the criminal cards to the DOM
+const contentContainer = document.querySelector(".criminalsContainer")
 export const CriminalList = () => {
     getCriminals()
     .then(() => {
         const criminalArray = useCriminals()
         let criminalHTMLRep = ""
-        const contentContainer = document.querySelector(".criminalsContainer")
+        
         for (const criminal of criminalArray) {
             criminalHTMLRep += CriminalHtml(criminal)
         }
@@ -15,3 +19,52 @@ export const CriminalList = () => {
     }
     )
 }
+
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".criminalsContainer")
+
+
+
+// Render ALL criminals initally
+// export const CriminalList = () => {
+//     getCriminals()
+//         .then(() => {
+//             const appStateCriminals = useCriminals()
+//             render(appStateCriminals)
+//         })
+// }
+
+// Listen for the custom event you dispatched in ConvictionSelect
+eventHub.addEventListener('crimeChosen', event => {
+        const appStateCriminals = useCriminals()
+        const convictionsArr = useConvictions()
+        const crimeId = convictionsArr.find(convictionObj => {
+            return convictionObj.id === parseInt(event.detail.crimeThatWasChosen)
+        })
+        console.log("crimeId", crimeId)
+    let filteredCriminalHtmlRep = ""
+    // Use the property you added to the event detail.
+    if (event.detail.crimeThatWasChosen !== "0"){
+        /*
+            Filter the criminals application state down to the people that committed the crime
+        */
+        const matchingCriminals = appStateCriminals.filter(currentCriminal => {
+            return currentCriminal.conviction === crimeId.name
+        })
+        console.log("Matching criminals: ", matchingCriminals)
+        /*
+            Then invoke render() and pass the filtered collection as
+            an argument
+        */
+       for (const criminal of matchingCriminals) {
+        filteredCriminalHtmlRep += CriminalHtml(criminal)
+    }
+    contentContainer.innerHTML = `<h2>Criminals:</h2> <p> ${filteredCriminalHtmlRep}</p>`
+    }    
+})
+
+const render = criminalCollection => {
+    contentTarget.innerHTML = you_fill_this_in
+}
+
+
