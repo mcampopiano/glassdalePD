c29c4b837c5860941ad51f229f69b16ebcd21ee2
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-21 10:58:57 -0500

    Wrote a function in officerSelect.js that renders a dropdown list in the DOM with each officer listed as an option

--- scripts/criminals/criminalList.js
+++ scripts/criminals/criminalList.js
@@ -2,10 +2,11 @@ import {getCriminals, useCriminals} from "./criminalProvider.js"
 import {CriminalHtml} from "./criminal.js"
 import { useConvictions } from "../convictions/ConvictionProvider.js"
 
-// ******************MY CODE BEFORE REFACTORING FOR EVENT HUB ****************
+// We need a reference to the eventHub in each module/component that will need to either listen or dispatch to it
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".criminalsContainer")
 
 // // renders the criminal cards to the DOM
-const contentContainer = document.querySelector(".criminalsContainer")
 export const CriminalList = () => {
     getCriminals()
     .then(() => {
@@ -15,24 +16,11 @@ export const CriminalList = () => {
         for (const criminal of criminalArray) {
             criminalHTMLRep += CriminalHtml(criminal)
         }
-        contentContainer.innerHTML = `<h2>Criminals:</h2> <p> ${criminalHTMLRep}</p>`
+        contentTarget.innerHTML = `<h2>Criminals:</h2> <p> ${criminalHTMLRep}</p>`
     }
     )
 }
 
-const eventHub = document.querySelector(".container")
-const contentTarget = document.querySelector(".criminalsContainer")
-
-
-
-// Render ALL criminals initally
-// export const CriminalList = () => {
-//     getCriminals()
-//         .then(() => {
-//             const appStateCriminals = useCriminals()
-//             render(appStateCriminals)
-//         })
-// }
 
 // Listen for the custom event you dispatched in ConvictionSelect
 eventHub.addEventListener('crimeChosen', event => {
@@ -52,19 +40,11 @@ eventHub.addEventListener('crimeChosen', event => {
             return currentCriminal.conviction === crimeId.name
         })
         console.log("Matching criminals: ", matchingCriminals)
-        /*
-            Then invoke render() and pass the filtered collection as
-            an argument
-        */
        for (const criminal of matchingCriminals) {
         filteredCriminalHtmlRep += CriminalHtml(criminal)
     }
-    contentContainer.innerHTML = `<h2>Criminals:</h2> <p> ${filteredCriminalHtmlRep}</p>`
+    contentTarget.innerHTML = `<h2>Criminals:</h2> <p> ${filteredCriminalHtmlRep}</p>`
     }    
 })
-
-const render = criminalCollection => {
-    contentTarget.innerHTML = you_fill_this_in
-}
 
 
--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js"
 import {CriminalList} from "./criminals/criminalList.js"
 import { OfficerList } from "./officers/officerList.js"
+import { OfficerSelect } from "./officers/officerSelect.js"
 CriminalList()
 ConvictionSelect()
 OfficerList()
+OfficerSelect()
