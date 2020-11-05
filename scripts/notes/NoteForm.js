800494f1e574f429c8157d88f555f816f6990bf1
Mario Campopiano <mcampopiano92@gmail.com>
2020-11-05 10:37:44 -0600

    changed the note form so that instead of an input field to select a criminal there is a dropdown with the list of criminals from the glassdale api

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,15 +1,21 @@
 import { saveNote } from "./NoteDataProvider.js"
+import {getCriminals, useCriminals} from "../criminals/criminalProvider.js"
 
 const contentContainer = document.querySelector(".noteFormContainer")
 const eventHub = document.querySelector(".container")
 
-const render = () => {
+const render = (suspectArr) => {
     contentContainer.innerHTML = `
     <section class="form">
     <input id="note--dateOfInterview" type="date" />
     <input id="note--case"type="text" placeholder="Case number" />
     <input id="note--author" type="text" placeholder="Author name" />
-    <input id="note--suspect" type="text" placeholder="Suspect name" />
+    <select id="noteForm--criminal" class="criminalSelect">
+    <option value="0">Please select a suspect</option>
+    ${suspectArr.map(criminal => {
+        return `<option value="${criminal.id}">${criminal.name}</option>`
+    })}
+    </select>
     <textarea id="note--note" placeholder="Enter note"></textarea>
     <button id="saveNote">Save Note</button>
     </section>`
@@ -17,7 +23,11 @@ const render = () => {
 }
 
 export const NoteForm = () => {
-    render()
+    getCriminals()
+    .then(() => {
+
+        render(useCriminals())
+    })
 }
 
 eventHub.addEventListener("click", event => {
