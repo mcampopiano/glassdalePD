d61455fef8580dfea188c0c35f030624511e49c5
Mario Campopiano <mcampopiano92@gmail.com>
2020-11-09 15:41:47 -0600

    Created data providers for facilities and criminal facilities, checked that they worked.

---
+++ scripts/facility/FacilityProvider.js
@@ -0,0 +1,9 @@
+let facilities = []
+
+export const useFacilities = () => facilities.slice()
+
+export const getFacilities = () => {
+    return fetch("https://criminals.glassdale.us/facilities")
+    .then(response => response.json())
+    .then(parsedFacilities => facilities = parsedFacilities)
+}
