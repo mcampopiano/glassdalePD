c90f1acfac3523dec63ee9b913954ce12dc7abe4
Mario Campopiano <mcampopiano92@gmail.com>
2020-11-11 11:04:18 -0600

    wrote functions so that each criminal card also displays a list of facilities they've been held in

--- scripts/criminals/criminalList.js
+++ scripts/criminals/criminalList.js
@@ -2,6 +2,8 @@ import {getCriminals, useCriminals} from "./criminalProvider.js"
 import {CriminalHtml} from "./criminal.js"
 import { useConvictions } from "../convictions/ConvictionProvider.js"
 import { useOfficers } from "../officers/officerProvider.js"
+import { getFacilities, useFacilities } from "../facility/FacilityProvider.js"
+import { getCriminalFacilities, useCriminalFacilities } from "../facility/CriminalFacilityProvider.js"
 
 // We need a reference to the eventHub in each module/component that will need to either listen or dispatch to it
 const eventHub = document.querySelector(".container")
@@ -10,19 +12,39 @@ const contentTarget = document.querySelector(".criminalsContainer")
 // // renders the criminal cards to the DOM
 export const CriminalList = () => {
     getCriminals()
+    .then(getFacilities)
+    .then(getCriminalFacilities)
     .then(() => {
         const criminalArray = useCriminals()
-        let criminalHTMLRep = ""
+        const facilities = useFacilities()
+        const crimFacilities = useCriminalFacilities()
+        // let criminalHTMLRep = ""
         
-        for (const criminal of criminalArray) {
-            criminalHTMLRep += CriminalHtml(criminal)
-        }
-        contentTarget.innerHTML = `<h2>Criminals:</h2> <p> ${criminalHTMLRep}</p>`
+        // for (const criminal of criminalArray) {
+        //     criminalHTMLRep += CriminalHtml(criminal)
+        // }
+        // contentTarget.innerHTML = `<h2>Criminals:</h2> <p> ${criminalHTMLRep}</p>`
+        render(criminalArray, facilities, crimFacilities)
     }
     )
 }
 
-
+const render = (criminalArr, facilitiesArr, crimFacilitiesArr) => {
+    debugger
+    let criminalHTMLRep = ""
+    criminalHTMLRep = criminalArr.map(criminal => {
+        // First take the criminal object and find the matching objects in the join table
+        const facilitiesForCriminal = crimFacilitiesArr.filter(fr => fr.criminalId === criminal.id)
+        // then take those join table objects and match them to the individual facility objects. Needs to iterate and use .find instead of filter because each relationship obj will only match to one facility obj
+        const facilities = facilitiesForCriminal.map(cf => {
+            const matchingFacilityObj = facilitiesArr.find(facility => facility.id === cf.facilityId)
+            return matchingFacilityObj
+        })
+        return CriminalHtml(criminal, facilities)
+    }).join("")
+    contentTarget.innerHTML = `<h2>Criminals:</h2> 
+    <p>${criminalHTMLRep}</p>`
+}
 // Listen for the custom event you dispatched in ConvictionSelect
 eventHub.addEventListener('crimeChosen', event => {
         const appStateCriminals = useCriminals()
